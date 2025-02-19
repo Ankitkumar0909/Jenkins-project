5b356103526d3eda980802170f2b5cfdcccba793
Ankitkumar0909 <148781551+Ankitkumar0909@users.noreply.github.com>
2025-02-19 17:35:43 +0530

    Create Apptest.java

---
+++ app/src/test/java/org/example/Apptest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+    @Test
+    void testBasic() {
+        assertEquals(1, 1, "Basic test passed");
+    }
+}
